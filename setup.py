5dbfc4ae1878d83d60379517212261d2f411eeeb
Jacob Kaplan-Moss <jacob@jacobian.org>
2009-10-12 10:20:12 -0500

    Add pony_client back to setup.py.

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(name='pony-build',
       url = 'http://github.com/ctb/pony-build',
       license = 'BSD',
       packages = ['pony_build'],
-      test_suite = 'nose.collector'
-#      py_modules = ['client/pony_build_client'],
-      )
+      test_suite = 'nose.collector',
+      py_modules = ['client/pony_client'],
+)
